15bbdc2e216715e888d495994e52f746cf6be924
20146519 <20146519@tafe.wa.edu.au>
2025-06-16 18:42:19 +0800

    Implement config.json support with parser for location and bay settings

--- smartpark/config_parser.py
+++ smartpark/config_parser.py
@@ -33,7 +33,14 @@ Finally, you can use `yaml` if you prefer.
 
 
 
-def parse_config(config: dict) -> dict:
-    """Parse the config file and return the values as a dictionary"""
-    # TODO: get the configuration from a parsed file
-    return {'location': 'TBD', 'total_spaces': 0, 'broker_host': 'TBD', 'broker_port': 0}
+#def parse_config(config: dict) -> dict:
+#    """Parse the config file and return the values as a dictionary"""
+#     TODO: get the configuration from a parsed file
+#   return {'location': 'TBD', 'total_spaces': 0, 'broker_host': 'TBD', 'broker_port': 0}
+
+import json
+
+def parse_config(file_path='config.json') -> dict:
+    """Load configuration from a JSON file and return as dictionary."""
+    with open(file_path, 'r') as file:
+        return json.load(file)
