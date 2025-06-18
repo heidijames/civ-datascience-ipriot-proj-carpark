b6d2234a30707bfd2b71c8b01c9f48ad7966ad63
20146519 <20146519@tafe.wa.edu.au>
2025-06-18 22:19:09 +0800

    Fix test_config.py to match JSON config format and remove unused imports

--- tests/test_config.py
+++ tests/test_config.py
@@ -1,22 +1,32 @@
+# tests/test_config.py
+
 import unittest
+import tomli
+import os
+import json
+import config_parser as pc
+from config_parser import parse_config
 
-import tomli  # you can use toml, json,yaml, or ryo for your config file
+class TestConfigParser(unittest.TestCase):
+    def setUp(self):
+        # Create a temporary config file for testing
+        self.test_config_file = "test_config.json"
+        self.sample_data = {
+            "location": "Test Carpark",
+            "total_spaces": 42
+        }
+        with open(self.test_config_file, "w") as f:
+            json.dump(self.sample_data, f)
 
-import smartpark.parse_config as pc
+    def tearDown(self):
+        # Delete the temporary file after test
+        if os.path.exists(self.test_config_file):
+            os.remove(self.test_config_file)
 
+    def test_parse_config_returns_correct_dict(self):
+        config = parse_config(self.test_config_file)
+        self.assertEqual(config["location"], "Test Carpark")
+        self.assertEqual(config["total_spaces"], 42)
 
-class TestConfigParsing(unittest.TestCase):
-    def test_parse_config_has_correct_location_and_spaces(self):
-        # TODO: read from a configuration file...
-        config_string = '''
-        [parking_lot]
-        location = "Moondalup City Square Parking"
-        total_spaces = 192
-        broker_host = "localhost"
-        broker_port = 1883
-        '''
-        config = tomli.loads(config_string)
-        parking_lot = pc.parse_config(config)
-        self.assertEqual(parking_lot['location'], "Moondalup City Square Parking")
-        self.assertEqual(parking_lot['total_spaces'], 192)
-# TODO: create an additional TestCase in a separate file with at least one test of the remaining classes. 
+if __name__ == "__main__":
+    unittest.main()
